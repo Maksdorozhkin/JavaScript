f0424386522dbc0e61abf6fd3f92da32410dfe53
Maksim Dorozhkin <59293973+Maksdorozhkin@users.noreply.github.com>
2024-04-16 20:46:53 +0300

    Объявление переменных и констант

--- variables.js
+++ variables.js
@@ -1,16 +1,13 @@
-// let
+// let присвоение переменной
 let a = 10
 a = 20 
 console.log(a)
-
 let b 
 b = false
-
 console.log(b)
 
 
-// const 
-
+// const присвоение констант значение присваивается в одной строке  
 const c = 10
 console.log(c)
 c = 20 
