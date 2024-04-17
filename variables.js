10e918967eb6d0ff6e8a5712265f6da54e5b31c0
Maksim Dorozhkin <59293973+Maksdorozhkin@users.noreply.github.com>
2024-04-17 12:46:12 +0300

    Ссылочный тип данных

--- variables.js
+++ variables.js
@@ -21,3 +21,21 @@ const c = 10
 console.log(c)
 // c = 20  // нельзя присваивать новые значения переменным объявленным с помощью const 
 console.log(c)
+
+
+// ссылочные типы данных
+
+const objectA = {
+    a: 10,
+    b: true
+}
+
+// создадим копию переменной 
+const copyOfA = objectA
+
+// меняем свойство объекта через ссылку
+copyOfA.a = 20
+console.log(objectA.a)
+// добавляем новое свойство объекту
+copyOfA.c = 'abc'
+console.log(objectA)
