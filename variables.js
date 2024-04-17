3b7e96f578fc85c2014d9ab0abf3f2f0b000e114
Maksim Dorozhkin <59293973+Maksdorozhkin@users.noreply.github.com>
2024-04-17 12:03:39 +0300

    Объявление переменных, констант

--- variables.js
+++ variables.js
@@ -7,8 +7,17 @@ b = false
 console.log(b)
 
 
+let myName
+console.log(myName)
+
+myName = 'Maks' // присваиваем значение переменной 
+console.log(myName)
+
+
+
+
 // const присвоение констант значение присваивается в одной строке  
 const c = 10
 console.log(c)
-c = 20 
+// c = 20  // нельзя присваивать новые значения переменным объявленным с помощью const 
 console.log(c)
