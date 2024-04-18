fd0309fd340191eec26866cc33566eed17e9c379
Maksim Dorozhkin <59293973+Maksdorozhkin@users.noreply.github.com>
2024-04-18 12:11:45 +0300

    КАК ИЗБЕЖАТЬ МУТАЦИЙ ОБЪЕКТОВ

--- object.js
+++ object.js
@@ -69,3 +69,48 @@ const myWorckUMT = {
 	}
 }
 myWorckUMT.myPosition()
+
+// КАК ИЗБЕЖАТЬ МУТАЦИЙ ОБЪЕКТОВ используем Object.assign({}, person) создаем новый объект на основании корневых свойств person (если нет вложеных объектов)
+const person = {
+	name: "Maks",
+	age: 43
+}
+
+const person2 = Object.assign({}, person)
+person2.age = 44
+console.log(person2.age)
+console.log(person.age)
+
+
+// вариант 2 немного проще используем { ...moto }, но по прежнему мутируется вложенный объект
+const moto = {
+	model: "Monster S4R",
+	age: 2004,
+	characteristics: {
+		volumeEngine: 996,
+		color: 'Red',
+		basrer: true
+	}
+}
+
+const myMoto = { ...moto }
+myMoto.characteristics.maxspeed = 240
+console.log(moto)
+console.log(myMoto)
+myMoto.characteristics.maxspeed = 220
+console.log(moto)
+myMoto.age = 2003
+console.log(moto)
+
+// вариант 3, позволяет польностью избежать мутаций включая вложенные объекты JSON.parse(JSON.stringify()) 
+const newPerson = {
+	name: 'Andrew',
+	age: 100
+}
+
+const newPerson2 = JSON.parse(JSON.stringify(newPerson))
+
+newPerson2.name = 'Katya'
+
+console.log(newPerson)
+console.log(newPerson2)
