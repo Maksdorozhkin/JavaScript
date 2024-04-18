a83e594190360b2ee8ff092e8390dbba5126db90
Maksim Dorozhkin <59293973+Maksdorozhkin@users.noreply.github.com>
2024-04-18 20:52:02 +0300

    КОЛБЭК ФУНКЦИЯ

--- Functions/main.js
+++ Functions/main.js
@@ -1,38 +1,39 @@
 // ФУНКЦИИ
 // Пример создания простой функции
-let a = 5
-let b = 3
-function sum (a,b) {
-	const c = a + b
-	console.log(c)
+let a = 5;
+let b = 3;
+function sum (a,b) 
+{
+  const c = a + b
+  console.log(c)
 }
 
-sum(a,b)
+sum(a,b);
 
-a = 100
-b = 150
+a = 100;
+b = 150;
 
-sum(a,b)
+sum(a,b);
 
 // Объявление и вызов функции (если в теле функции нет return функция вернет undefined)
 function myFun(a,b) {
-	let c 
-	a = a + 1
-	c = a + b 
-	return c
+  let c 
+  a = a + 1
+  c = a + b 
+  return c
 }
 
 console.log(myFun(10.2, 40.36))
 
 // ПЕРЕДАЧА ЗНАЧЕНИЯ ПО ССЫЛКЕ, a и personeOne будут ссылаться на один объект (ВНУТРИ ФУНКЦИИ НЕ РЕКОМЕНДУЕТСЯ МЕНЯТЬ ВНЕШНИЕ ОБЪЕКТЫ)
 const personOne = {
-	name: 'Maks',
-	age: 43
+  name: 'Maks',
+  age: 43
 } 
 
 function increasePersonAge(a) {
-	a.age += 1
-	return a
+  a.age += 1
+  return a
 }
 
 increasePersonAge(personOne)
@@ -40,13 +41,17 @@ console.log(personOne.age)
 
 // ДЕЛАЕМ КОПИЮ ОБЪЕКТА, ВНУТРИ ФУНКЦИИ создаем новый объект
 function increasePersonAge2 (b) {
-	const updatePerson = { ...b }//делаю копию объекта
-	updatePerson.age += 1
-	return updatePerson
+  const updatePerson = { ...b }//делаю копию объекта
+  updatePerson.age += 1
+  return updatePerson
 }
 
 const updatePersonOne = increasePersonAge2(personOne)
 console.log(personOne.age)
 console.log(updatePersonOne.age)
 
-
+// КОЛБЭК ФУНКЦИЯ
+function printMyName() {
+  console.log('Maksim')
+}
+setTimeout(printMyName, 500) // вызывает колбэк функцию через 500 миллисекунд  
