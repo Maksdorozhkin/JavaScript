b6d1c3a99729a4633636fb943c6ec23d0d2aaeb6
Maksim Dorozhkin <59293973+Maksdorozhkin@users.noreply.github.com>
2024-04-19 14:23:46 +0300

    оператор и &&, оператор или ||

--- operators.js
+++ operators.js
@@ -1,19 +1,28 @@
 //  Оператор не (!)
-console.log(!10)
-console.log(!0)
-console.log(!'abc')
-console.log(!'')
-console.log(!true)
-console.log(!undefined)
+console.log(!10);
+console.log(!0);
+console.log(!"abc");
+console.log(!"");
+console.log(!true);
+console.log(!undefined);
 
-console.log("")
+console.log("");
 // оператор отрицания отрицания (!!)
-console.log(!!10)
-console.log(!!0)
-console.log(!!'abc')
-console.log(!!'')
-console.log(!!true)
-console.log(!!undefined)
+console.log(!!10);
+console.log(!!0);
+console.log(!!"abc");
+console.log(!!"");
+console.log(!!true);
+console.log(!!undefined);
 
-let a // пустая переменная 
-console.log(!!a)
+let a; // пустая переменная
+console.log(!!a);
+
+console.log("");
+// операторы короткого замыкания, оператор и (&&)
+// выражение 1 && выражение 2 - если выражение 1 ложно то выражение 2 игнорируется(возвращает ложное выражение)
+console.log(0 && "Maks");
+
+// ОПЕРАТОР ИЛИ (||)
+// если выражение 1 истинно то выражение 2 игнорируется (возвращает результат истинного выражения)
+console.log("Maks" || "");
