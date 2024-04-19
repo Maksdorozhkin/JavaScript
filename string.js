3897dd0c8ee0ad124c076e51bda16e28b0140b50
Maksim Dorozhkin <59293973+Maksdorozhkin@users.noreply.github.com>
2024-04-19 17:08:46 +0300

    Шаблонные строки используем обратные кавычки

---
+++ string.js
@@ -0,0 +1,12 @@
+// Шаблонные строки используем обратные кавычки
+const hello = "Hello";
+const world = "World";
+const greeting = `${hello} ${world}`; // пример шаблонной строки
+console.log(greeting);
+
+// задание
+const myName = "Maks";
+const myCity = "Dedovsk";
+console.log(
+  `My name is ${myName}, i living in ${myCity.toLocaleLowerCase()} now`
+);
