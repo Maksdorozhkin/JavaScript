e2c22566b056897c6c2e52dfe39892ff92576569
Maksim Dorozhkin <59293973+Maksdorozhkin@users.noreply.github.com>
2024-04-19 18:44:01 +0300

    Значение параметров функции по умолчанию (в данном случае значение multiplier = 1)

--- Functions/main.js
+++ Functions/main.js
@@ -2,56 +2,89 @@
 // Пример создания простой функции
 let a = 5;
 let b = 3;
-function sum (a,b) 
-{
-  const c = a + b
-  console.log(c)
+function sum(a, b) {
+  const c = a + b;
+  console.log(c);
 }
 
-sum(a,b);
+sum(a, b);
 
 a = 100;
 b = 150;
 
-sum(a,b);
+sum(a, b);
 
 // Объявление и вызов функции (если в теле функции нет return функция вернет undefined)
-function myFun(a,b) {
-  let c 
-  a = a + 1
-  c = a + b 
-  return c
+function myFun(a, b) {
+  let c;
+  a = a + 1;
+  c = a + b;
+  return c;
 }
 
-console.log(myFun(10.2, 40.36))
+console.log(myFun(10.2, 40.36));
 
 // ПЕРЕДАЧА ЗНАЧЕНИЯ ПО ССЫЛКЕ, a и personeOne будут ссылаться на один объект (ВНУТРИ ФУНКЦИИ НЕ РЕКОМЕНДУЕТСЯ МЕНЯТЬ ВНЕШНИЕ ОБЪЕКТЫ)
 const personOne = {
-  name: 'Maks',
-  age: 43
-} 
+  name: "Maks",
+  age: 43,
+};
 
 function increasePersonAge(a) {
-  a.age += 1
-  return a
+  a.age += 1;
+  return a;
 }
 
-increasePersonAge(personOne)
-console.log(personOne.age)
+increasePersonAge(personOne);
+console.log(personOne.age);
 
 // ДЕЛАЕМ КОПИЮ ОБЪЕКТА, ВНУТРИ ФУНКЦИИ создаем новый объект
-function increasePersonAge2 (b) {
-  const updatePerson = { ...b }//делаю копию объекта
-  updatePerson.age += 1
-  return updatePerson
+function increasePersonAge2(b) {
+  const updatePerson = { ...b }; //делаю копию объекта
+  updatePerson.age += 1;
+  return updatePerson;
 }
 
-const updatePersonOne = increasePersonAge2(personOne)
-console.log(personOne.age)
-console.log(updatePersonOne.age)
+const updatePersonOne = increasePersonAge2(personOne);
+console.log(personOne.age);
+console.log(updatePersonOne.age);
 
 // КОЛБЭК ФУНКЦИЯ
 function printMyName() {
-  console.log('Maksim')
+  console.log("Maksim");
 }
-setTimeout(printMyName, 500) // вызывает колбэк функцию через 500 миллисекунд  
+setTimeout(printMyName, 500); // вызывает колбэк функцию через 500 миллисекунд
+
+// Значение параметров функции по умолчанию (в данном случае значение multiplier = 1)
+function multByFactor(value, multiplier = 1) {
+  return value * multiplier;
+}
+
+console.log("");
+console.log(multByFactor(10, 2));
+console.log(multByFactor(5));
+
+// функциональное выражение
+const multByFactor1 = function (value, multiplier = 1) {
+  return console.log(value * multiplier);
+};
+
+multByFactor1(6);
+
+// arrow
+const multByFactor2 = (value, multiplier = 1) => {
+  return console.log(value * multiplier);
+};
+multByFactor2(10);
+
+//  Интересный пример возвращает объект с текущей датой
+const newPost = (post, addetAT = Date()) => ({
+  ...post,
+  addetAT,
+});
+const firstPost = {
+  id: 1,
+  author: "Maks",
+};
+
+console.log(newPost(firstPost));
