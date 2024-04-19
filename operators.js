b9a7093cf7eab63db078cb096ffbe272a44b069e
Maksim Dorozhkin <59293973+Maksdorozhkin@users.noreply.github.com>
2024-04-19 13:54:53 +0300

    оператор (!) и (!!)

---
+++ operators.js
@@ -0,0 +1,19 @@
+//  Оператор не (!)
+console.log(!10)
+console.log(!0)
+console.log(!'abc')
+console.log(!'')
+console.log(!true)
+console.log(!undefined)
+
+console.log("")
+// оператор отрицания отрицания (!!)
+console.log(!!10)
+console.log(!!0)
+console.log(!!'abc')
+console.log(!!'')
+console.log(!!true)
+console.log(!!undefined)
+
+let a // пустая переменная 
+console.log(!!a)
