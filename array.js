1cf8db59a66c32b243198cb5eb3f3e5d62b226d3
Maksim Dorozhkin <59293973+Maksdorozhkin@users.noreply.github.com>
2024-04-21 15:55:32 +0300

    Array, методы map, forEach, pop, unshift

--- array.js
+++ array.js
@@ -22,8 +22,30 @@ console.log(removedElement);
 // добавление в начало массива
 myArray.unshift(true);
 myArray.unshift("abc");
-console.log(myArray);
 
 //удаляет первый элемент по аналогии с pop
 const removedElement1 = myArray.shift();
 console.log(removedElement1);
+
+//метод foreach перебирает массив ( возвращает undefined)
+console.log(myArray);
+myArray2.forEach((el) => console.log(el * 3)); // данный код умножает каждый элемент на 3 и делает перебор при этом не меняет оригинальный массив
+console.log(myArray);
+
+const res = myArray.forEach((zx) => console.log(zx)); // простой перебор
+console.log(res);
+
+// метод map (возвращает новый массив)
+const returnMyArray = myArray2.map((x) => x * 10); // перебирает элементы массива и в данном примере применяет стрелочную функцию (возвращает новый массив)
+console.log(returnMyArray);
+
+// используем функциональное выражение вместо стрелочной функции
+const returnMyArray3 = myArray2.map(function (x) {
+  return x * 10;
+});
+console.log(returnMyArray3);
+
+const newReturnMyArray = myArray2.map((x) => {
+  return x * 20; // без return вернет undefined (неявно возвращать можно только в стрелочной функции)
+});
+console.log(newReturnMyArray);
