77dd0a6331011e1e5ab4ad6ae5bce20bdde5ef2f
Maksim Dorozhkin <59293973+Maksdorozhkin@users.noreply.github.com>
2024-04-23 10:57:20 +0300

    while, do while

---
+++ cycle/while.js
@@ -0,0 +1,15 @@
+// WHILE
+let i = 0;
+while (i < 5) {
+  console.log(i);
+  i++; //* без этой строки цикл будет выполнятся бесконечно
+}
+
+console.log("");
+
+// DO WHILE
+let j = 5;
+do {
+  console.log(j);
+  j++;
+} while (j < 11);
