536eb425983ce6c9516f65e415ed14c112c3de12
Maksim Dorozhkin <59293973+Maksdorozhkin@users.noreply.github.com>
2024-04-24 11:28:15 +0300

    Импорт и экспорт модулей

--- modules/moduleOne.mjs
+++ modules/moduleOne.mjs
@@ -1,3 +1,12 @@
 const sum = (a, b) => a + b;
 
-export default sum;
+//экспорт нескольких переменных
+const one = 1;
+const two = "two";
+
+export { one, two };
+
+//
+const mult = (a, b) => a * b;
+
+export { sum, mult };
--- modules/moduleTwo.mjs
+++ modules/moduleTwo.mjs
@@ -1,5 +1,14 @@
-import sumNumbers from "./moduleOne.mjs";
+import { sum, mult } from "./moduleOne.mjs";
+import { one as oneRenamed, two } from "./moduleOne.mjs";
 
-const res1 = sumNumbers(10, 2);
+const res1 = sum(10, 2);
 console.log(res1);
-console.log(sumNumbers(12, 15));
+console.log(sum(12, 15));
+
+//Импорт нескольких переменных //! переименование импортированных переменных line 2
+console.log(oneRenamed);
+console.log(two);
+
+//
+console.log(sum(10, 10));
+console.log(mult(10, 10));
