70b18a1fbbe278956b314c08b196650c21c84c19
Maksim Dorozhkin <59293973+Maksdorozhkin@users.noreply.github.com>
2024-04-28 11:22:11 +0300

    Переместил по папкам

--- bogdan course JavaScript/TASKS/36-random-numbers/finish.js
+++ bogdan course JavaScript/TASKS/36-random-numbers/finish.js
@@ -14,7 +14,17 @@
  *  - для добавления случайного числа в массив с возвратом измененного массива
  */
 
-const MIN = 1000
-const MAX = 9999
+const MIN = 1000;
+const MAX = 9999;
 
-const myNumbers = [2355, 7235, 8135, 1762, 2361, 8351]
+const myNumbers = [2355, 7235, 8135, 1762, 2361, 8351];
+
+const randomNumber = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
+let myArray = [];
+const a = myNumbers.forEach((elem) => {
+  if (elem != randomNumber) {
+    myArray.push(randomNumber);
+  }
+});
+
+console.log(myNumbers, myArray);
